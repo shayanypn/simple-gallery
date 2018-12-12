71c211f5ae88f66551ad3b0e1ef89071441803a4
shayanypn <shayanypn@gmail.com>
2018-12-12 09:18:36 +0330

    first initial

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/app.scss';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div>hello</div>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const nodeModules = path.resolve(path.join(__dirname, 'node_modules'));
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(css)$/,
+        use: [
+          {loader:'style-loader'},
+          {loader:'css-loader'},
+        ]
+      },{
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+      }
+      
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
